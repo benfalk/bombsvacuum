27fb0410125e90fe6a6ab22b427b2f23d1522edb
Benjamin Falk <benjamin.falk@yahoo.com>
2013-06-21 16:29:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BombsVacuum::Application.config.secret_key_base = '499bf36e3737ea10bb658c90a9c87d2c9c955953f23ce417290eb6f42315bd3530f9889ab9e43c41d4cb9c7d208f5ea9a111aed60c7b10ec04479b3f0e765714'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BombsVacuum::Application.config.session_store :cookie_store, key: '_bombs_vacuum_session'
