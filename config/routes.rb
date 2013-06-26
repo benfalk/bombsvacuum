f9f09d6e573ab6f90054bc2a403ae12280c632b7
Brad Potterbaum <bpotterbaum@gmail.com>
2013-06-26 10:17:33 -0500

    adding back route updates from ben

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ BombsVacuum::Application.routes.draw do
 
   devise_for :users
 
+  resources :fields, :except => [:edit,:update,:patch] do
+    resources :locations, :only => [:patch,:update,:show]
+  end
+
   # You can have the root of your site routed with "root"
   root 'fields#index'
 
