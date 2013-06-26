6b9c74c7e4d61579798482823ff55fd15ce0f7ac
Brad Potterbaum <bpotterbaum@gmail.com>
2013-06-26 08:51:44 -0500

    adding in dvise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 BombsVacuum::Application.routes.draw do
 
-
-  resources :fields, :except => [:edit,:update,:patch] do
-    resources :locations, :only => [:patch,:update,:show]
-  end
+  devise_for :users
 
   # You can have the root of your site routed with "root"
-  root 'board#index'
-
-
-  # root 'fields#index'
+  root 'fields#index'
 
 end
