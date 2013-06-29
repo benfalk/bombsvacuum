c783b9bbc7ddf1072d910e958b748d465813e8f4
Benjamin Falk <benjamin.falk@yahoo.com>
2013-06-29 08:52:09 -0500

    Putting in the tie of a user to the locations they update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130629053129) do
+ActiveRecord::Schema.define(version: 20130629130926) do
 
   create_table "fields", force: true do |t|
     t.integer  "width"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20130629053129) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "mines"
+    t.integer  "user_id"
   end
 
   create_table "sessions", force: true do |t|
