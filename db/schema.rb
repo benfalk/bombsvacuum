7776a51324d024403c7f5443c13c930b03ab4d15
Brad Potterbaum <bpotterbaum@gmail.com>
2013-06-29 00:44:32 -0500

    added guest user but its not quite finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130627130543) do
+ActiveRecord::Schema.define(version: 20130629053129) do
 
   create_table "fields", force: true do |t|
     t.integer  "width"
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 20130627130543) do
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20130627130543) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.boolean  "guest",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
